f365cf8b68795f479fb40f0d16456728968dc7ae
ReturnSweetPork <happylovetkd@naver.com>
2019-12-27 15:06:04 +0900

    async

--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types'
-import { render } from '@testing-library/react';
 
-class Movie extends Component{
 
-    static propTypes = {
-        title: PropTypes.string,
-        poster: PropTypes.string,
-    }
-    render(){
-        return(
+function Movie(title, poster){
+    return(
         <div>
-        <MoviePoster poster={this.props.poster}></MoviePoster>
-            <h1>{this.props.title}</h1>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
         </div>
-        )
-    }
+    )
 }
 
-class MoviePoster extends Component{
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        console.log(this.props);
-        return(
-            <img src={this.props.poster}></img>
-        )
-        }
+// dump state로 만든 MoviePoster
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster"></img>
+    )
 }
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+}
+
+
+
+
+
 export default Movie;
