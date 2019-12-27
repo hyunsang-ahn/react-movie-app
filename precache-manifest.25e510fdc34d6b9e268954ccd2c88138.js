c77d507be71c94818e1dcade6724034c98079906
ReturnSweetPork <happylovetkd@naver.com>
2019-12-27 16:57:15 +0900

    Updates

---
+++ precache-manifest.25e510fdc34d6b9e268954ccd2c88138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75066e870464db8006a808e1ddf74d60",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "249de9738a5917199062",
+    "url": "/react-movie-app/static/css/main.68556fdc.chunk.css"
+  },
+  {
+    "revision": "393ce1d207abd581e3e4",
+    "url": "/react-movie-app/static/js/2.d83474fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-movie-app/static/js/2.d83474fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "249de9738a5917199062",
+    "url": "/react-movie-app/static/js/main.c1014f1c.chunk.js"
+  },
+  {
+    "revision": "cf37fd05dd80c688b7a0",
+    "url": "/react-movie-app/static/js/runtime-main.25b6fc7f.js"
+  }
+]);
